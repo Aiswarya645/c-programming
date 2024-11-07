16681dea8bcada9dc25f685e0027b5c981593a70
Aiswarya C <caiswarya645@gmail.com>
2024-11-07 22:49:01 +0530

    Add files via upload

---
+++ 06-11-2024/main(1).c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("enter the first integer:");
+    scanf("%d",&a);
+    printf("enter the second integer:");
+    scanf("%d",&b);
+    if(a>b)
+        {
+            printf("a is greater integer %d",a);
+            
+        }
+        else
+           printf(" b is greater integer %d",b);
+
+    return 0;
+}
